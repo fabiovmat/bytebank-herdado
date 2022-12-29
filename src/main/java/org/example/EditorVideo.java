d2189a630984c0145a844ad8747e630b0b6e3e85
Fabio Matt <fabio.meihua08@gmail.com>
2022-12-29 19:05:32 -0300

    Update

---
+++ src/main/java/org/example/EditorVideo.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class EditorVideo extends  Funcionario {
+    public double getBonificacao() {
+        System.out.println("Chamando o método de bonificacao do Editor de Video");
+        return super.getBonificacao() + 100;
+
+    }
+
+}
--- src/main/java/org/example/Gerente.java
+++ src/main/java/org/example/Gerente.java
@@ -1,29 +1,26 @@
 package org.example;
 
-public class Gerente extends Funcionario{
-
+public class Gerente extends Funcionario {
 
     private int senha;
 
-    public Gerente() {
 
-    }
+
 
     public void setSenha(int senha) {
         this.senha = senha;
     }
 
-    public boolean autentica(int senha) {
-        if (senha == senha) {
+    public boolean autentica(String login ,int senha) {
+        if(this.senha == senha) {
             return true;
         } else {
             return false;
         }
-
     }
 
-//    public double getBonificacao() {
-//        return this.salario;
-//    }
-
+    public double getBonificacao() {
+        System.out.println("Chamando o método bonificacao do GERENTE");
+        return super.getBonificacao() + super.getSalario();
+    }
 }
